bb722aa45f6be51b2fab9174f64758cca634b916
Brian-Momata <brianmomata@gmail.com>
2022-07-15 15:50:22 +0300

    Add game function that tracks the score and reports the winner

--- script.js
+++ script.js
@@ -12,27 +12,69 @@ function randomIndex() {
 }
 
 function playRound (playerSelection, computerSelection) {
-    let playerWins = 'Fantasitic! You win this round';
-    let computerWins = 'Oops! You lost this round';
+
+    let playerWins = `Fantasitic! You win this round.\n${playerSelection} beats ${computerSelection} `;
+    let computerWins = `Oops! You lost this round.\n${computerSelection} beats ${playerSelection}`;
     let draw = `Draw! You both chose ${playerSelection}`;
     
     if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+
         return draw;
 
     }
     else if (playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() == 'scissors'){
-        return playerWins;
 
+        return playerWins;
     }
     else if (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() == 'rock'){
+        
         return playerWins;
-
     }
     else if (playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() == 'paper'){
-        return playerWins;
 
+        return playerWins;
     } 
     else  { 
         return computerWins;
     }
 }
+
+function game() {
+
+    let youWon = 'Congrats! You Beat the Computer';
+    let youLost = 'Dang it! The Computer Won';
+    let playerScore = 0;
+    let compScore = 0;
+    
+    for (let i = 0; i < 5; i++) {
+
+        const playerSelection = prompt('Choose between Rock, Paper and Scissors.');
+        const computerSelection = computerPlay();
+        let playerWins = `Fantasitic! You win this round.\n${playerSelection} beats ${computerSelection} `;
+        let computerWins = `Oops! You lost this round.\n${computerSelection} beats ${playerSelection}`;
+        let draw = `Draw! You both chose ${playerSelection}`;
+        
+        let result = playRound(playerSelection, computerSelection);
+        
+        if (result == playerWins) {
+            playerScore += 1;
+        }
+        if (result == computerWins) {
+            compScore += 1;
+        }
+        if (result == draw) {
+            playerScore += 0;
+            compScore += 0;
+        }
+        
+        console.log(playRound(playerSelection, computerSelection))
+    }
+    if (playerScore > compScore) {
+        console.log(`${youWon}. The computer took ${compScore} rounds while you took ${playerScore} rounds.`)
+    }
+    else {
+        console.log(`${youLost}. The computer took ${compScore} rounds while you took ${playerScore} rounds.`)
+    }
+}
+
+game();
