faa0874f8fec83a5894b37c9d58c69425b639854
Brian-Momata <brianmomata@gmail.com>
2022-07-25 11:20:05 +0300

    Add event listeners to the buttons that play the game

--- script.js
+++ script.js
@@ -1,90 +1,90 @@
-const choices = ['Rock','Paper','Scissors']; // An array of choices the player will pick
+let playerScore = 0;
+let compScore = 0;
+let count = 1;
+
+const choices = ['rock','paper','scissors'];
 
 function computerPlay() {
-// function picks a random index from the choices array:
   let result = choices[randomIndex()];
   return result;
 }
 
 function randomIndex() {
-    /* function decides how many items are in the choices array
-    and applies the .random() and .floor() method to randomly select an element */
-
     let index = Math.floor(Math.random() * choices.length);
     return index;
 }
 
+let roundResult = document.querySelector('#roundResult');
+let yourScore = document.querySelector('#playerscore');
+let computerScore = document.querySelector('#compscore');
+let gameStatus = document.querySelector('#gameStatus');
+
 function playRound (playerSelection, computerSelection) {
-    /* compares the selection of the player with that of the computer
-    and returns a winner of the round.
-    */
-
-    let playerWins = `Fantasitic! You win this round.\n${playerSelection} beats ${computerSelection} `;
-    let computerWins = `Oops! You lost this round.\n${computerSelection} beats ${playerSelection}`;
-    let draw = `Draw! You both chose ${playerSelection}`;
-    
-
-    if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
-
-        return draw;
-
-    }
-    else if (playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() == 'scissors'){
-
-        return playerWins;
-    }
-    else if (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() == 'rock'){
+    if (playerSelection === computerSelection) {
         
-        return playerWins;
+        roundResult.textContent = `Draw! You both chose ${computerSelection}`;
+        yourScore.textContent = `Your Score:${playerScore}`;
+        computerScore.textContent = `Computer Score:${compScore}`;
+        gameStatus.textContent = `Round:${count}`;
     }
-    else if (playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() == 'paper'){
-
-        return playerWins;
+    else if (playerSelection === 'rock' && computerSelection == 'scissors'){
+        playerScore += 1;
+        roundResult.textContent = `Fantastic! ${playerSelection} beats ${computerSelection}`;
+        yourScore.textContent = `Your Score:${playerScore}`;
+        computerScore.textContent = `Computer Score:${compScore}`;
+        gameStatus.textContent = `Round: ${count}`;
+    }
+    else if (playerSelection === 'paper' && computerSelection == 'rock'){
+        playerScore += 1;
+        roundResult.textContent = `Fantastic! ${playerSelection} beats ${computerSelection}`;
+        yourScore.textContent = `Your Score:${playerScore}`;
+        computerScore.textContent = `Computer Score:${compScore}`;
+        gameStatus.textContent = `Round: ${count}`;
+    }
+    else if (playerSelection === 'scissors' && computerSelection == 'paper'){
+        playerScore += 1;
+        roundResult.textContent = `Fantastic! ${playerSelection} beats ${computerSelection}`;
+        yourScore.textContent = `Your Score:${playerScore}`;
+        computerScore.textContent = `Computer Score:${compScore}`;
+        gameStatus.textContent = `Round: ${count}`;
     } 
     else  { 
-        return computerWins;
+        compScore += 1;
+        roundResult.textContent = `Oops! ${computerSelection} beats ${playerSelection}`;
+        yourScore.textContent = `Your Score:${playerScore}`;
+        computerScore.textContent = `Computer Score:${compScore}`;
+        gameStatus.textContent = `Round: ${count}`;
+    }
+    count++;
+    if (count > 5) {
+        gameStatus.textContent = playerScore > compScore ? 'AWESOME! You Won': 'Dang it! The computer won';
     }
 }
 
-function game() {
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+const resetButton = document.querySelector('.resetbutton');
 
-    /* Prompts the player to choose and object and plays 5 rounds
-    Updates score after every round and compares the final score to decide the winner  */
-
-    let youWon = 'Congrats! You Beat the Computer';
-    let youLost = 'Dang it! The Computer Won';
-    let playerScore = 0;
-    let compScore = 0;
-    
-    for (let i = 0; i < 5; i++) {
-
-        const playerSelection = prom('Choose between Rock, Paper and Scissors.');
-        const computerSelection = computerPlay();
-        let playerWins = `Fantasitic! You win this round.\n${playerSelection} beats ${computerSelection} `;
-        let computerWins = `Oops! You lost this round.\n${computerSelection} beats ${playerSelection}`;
-        let draw = `Draw! You both chose ${playerSelection}`;
-        
-        let result = playRound(playerSelection, computerSelection);
-
-        if (result == playerWins) {
-            playerScore += 1;
-        }
-        if (result == computerWins) {
-            compScore += 1;
-        }
-        if (result == draw) {
-            playerScore += 0;
-            compScore += 0;
-        }
-        
-        console.log(playRound(playerSelection, computerSelection))
-    }
-    if (playerScore > compScore) {
-        console.log(`${youWon}. The computer took ${compScore} rounds while you took ${playerScore} rounds.`)
-    }
-    else {
-        console.log(`${youLost}. The computer took ${compScore} rounds while you took ${playerScore} rounds.`)
-    }
+function resetGame() {
+    roundResult.textContent = '';
+    yourScore.textContent = '';
+    computerScore.textContent = '';
+    gameStatus.textContent = '';
+    compScore = 0;
+    playerScore = 0;
+    count = 0;
 }
+rock.addEventListener('click', function(){
+    playRound('rock', computerPlay());
+});
+paper.addEventListener('click', function(){
+    playRound('paper', computerPlay());
+});
+scissors.addEventListener('click', function(){
+    playRound('scissors', computerPlay());
+});
 
-game();
+resetButton.addEventListener('click', function(){
+    resetGame();
+});
